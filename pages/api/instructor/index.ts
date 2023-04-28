f2ce6a976321e55026c2a7fc630deb2c126ea5d8
Leonbart <leonardo.alabart@gmail.com>
2023-04-28 20:04:19 -0300

    feat(api/plane): Added Airplane model

---
+++ pages/api/instructor/index.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/pages/libs/prismadb';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // - GET Instructors------------------
+  if (req.method === 'GET') {
+
+    try {
+      // Use the Prisma Client to fetch all users with role 'INSTRUCTOR'
+      const instructorUsers = await prisma.user.findMany({
+        where: {
+          role: 'INSTRUCTOR'
+        }
+      });
+
+      // If there are no users with that role, return a 404 error
+      if (!instructorUsers) {
+        return res.status(404).json({ message: 'No users with role INSTRUCTOR found' });
+      }
+
+      // If users exist, return them as an array in a JSON response
+      res.json(instructorUsers);
+    }
+    catch (error) {
+      return res.status(500).json({ error });
+    }
+    finally {
+      await prisma.$disconnect();
+    }
+  }
+
+  else {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+}
