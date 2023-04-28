438a63a585030e3c6a7b692588e7224fc63c2062
Leo <leonardo.alabart@gmail.com>
2023-04-28 00:58:03 -0300

    schema.prisma synchronized with Hector's

--- pages/api/flight/index.ts
+++ pages/api/flight/index.ts
@@ -36,32 +36,23 @@ export default async function handler(
       console.error(error);
       return res.status(500).json({ message: `Error creating flight` });
     }
-
   }
+  
   // - GET ------------------
   else if (req.method === 'GET') {
-    console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
 
-    const { id } = req.query;
+    // Use the Prisma Client to fetch all flights data
+    const allFlights = await prisma.flight.findMany();
 
-    const url_id = '6449aaa475ccb3e96b89fdec';
-
-    const flightId = new ObjectId(id as string).toString();
-
-    // Use the Prisma Client to fetch the flight data
-    const flight = await prisma.flight.findUnique({
-      where: { id: flightId },
-    });
-
-    // If the flight does not exist, return a 404 error
-    if (!flight) {
-      return res.status(404).json({ message: 'Flight not found' });
+    // If there are no flights, return a 404 error
+    if (!allFlights) {
+      return res.status(404).json({ message: 'No flights found' });
     }
 
     // If the flight exists, return it as a JSON response
-    res.json(flight);
-
+    res.json(allFlights);
   }
+
   else {
     return res.status(405).json({ message: 'Method not allowed' });
   }
