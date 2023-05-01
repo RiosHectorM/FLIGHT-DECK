2190dd7323d3b532295ba5426395c9704dc0be63
Leo <leonardo.alabart@gmail.com>
2023-05-01 13:15:35 -0300

    feat(api/flight): Get filtered flights tested OK. Working with query

--- pages/api/flight/getFilteredFlights/index.ts
+++ pages/api/flight/getFilteredFlights/index.ts
@@ -2,11 +2,12 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '@/pages/libs/prismadb';
 
 
-type Filters = {
-  date?: string;
-  aircraftId?: string;
-  folio?: string;
-};
+// type Filters = {
+//   userId?: string;
+//   date?: string;
+//   aircraftId?: string;
+//   folio?: string;
+// };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   
@@ -14,25 +15,36 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'GET') {
 
     try {
-      const { date, aircraftId, folio }: Filters = req.body;
+      // const { userId, date, aircraftId, folio }: Filters = req.body;
+      const { userId, date, aircraftId, folio } = req.query;
+
+      console.log('AAAA----userId---: ' + req.query.userId);
+      console.log('EEEE----date-----: ' + req.query.date);
+      console.log('IIII--aircraftId-: ' + req.query.aircraftId);
+
 
       const filters = {
-        date: date ? new Date(date) : undefined,
+        userId: userId ? userId as string : undefined,
+        date: date ? new Date(date as string) : undefined,
         aircraftId: aircraftId ? aircraftId : undefined,
-        folio: folio ? parseInt(folio, 10) : undefined,
+        folio: folio ? parseInt(folio as string, 10) : undefined,
       };
+
+      console.log(filters);
+      
 
       // Use the Prisma Client to fetch all flights according to received filters
       const flights = await prisma.flight.findMany({
         where: {
           ...filters,
         },
-        include: {
-          user: true,
-        },
+        // include: {
+        //   user: true,
+        // },
       });
 
-      res.status(200).json({ flights });
+      // res.status(200).json({ flights });
+      res.status(200).json(flights);
     }
     catch (error) {
       return res.status(500).json({ error });
