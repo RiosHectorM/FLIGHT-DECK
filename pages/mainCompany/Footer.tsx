c66de8999b31b4882ba9fca8d97c97603ba53dc1
DALGO0927 <dieleon13@gmail.com>
2023-05-03 17:06:21 -0500

    Dashboard Company

--- pages/mainCompany/Footer.tsx
+++ pages/mainCompany/Footer.tsx
@@ -93,7 +93,7 @@ const Footer = () => {
       <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
         <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
           <div className="px-5 py-2">
-            <Link href="/">
+{/*             <Link href="/">
               <span className="text-base text-gray-500 hover:text-gray-900">Home</span>
             </Link>
           </div>
@@ -110,7 +110,7 @@ const Footer = () => {
           <div className="px-5 py-2">
             <Link href="/privacy">
               <span className="text-base text-gray-500 hover:text-gray-900">Privacy Policy</span>
-            </Link>
+            </Link */}>
           </div>
         </nav>
         <p className="mt-8 text-center text-base text-gray-400">&copy; 2023 My Company, Inc. All rights reserved.</p>
