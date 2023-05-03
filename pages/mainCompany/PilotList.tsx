b3b367e815308b7daef4f7e8a6ad6ec5ead3688c
DALGO0927 <dieleon13@gmail.com>
2023-05-03 17:03:37 -0500

    dasboard company y funcionalidad en ver detalles de pilotos

--- pages/mainCompany/PilotList.tsx
+++ pages/mainCompany/PilotList.tsx
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import PilotDetails from './PilotDetails';
 
 interface Pilot {
   name: string;
   id: string;
+  photoUrl: string;
+  location: string;
+  hoursOfFlight: number;
+  aircraftType: string;
+  availability: string;
+  bio: string;
+  flightLogUrl: string;
+  email:string;
 }
 
 const PilotList: React.FC = () => {
   const [pilots, setPilots] = useState<Pilot[]>([]);
+  const [selectedPilot, setSelectedPilot] = useState<Pilot | null>(null);
 
   useEffect(() => {
     async function fetchPilots() {
@@ -19,15 +29,21 @@ const PilotList: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center space-y-4">
-      <h2 className="text-lg font-semibold text-white">Lista de pilotos</h2>
+      <h2 className="text-lg font-semibold text-blue">Lista de pilotos</h2>
       <ul className="divide-y divide-white">
         {pilots.map((pilot) => (
           <li className="flex justify-between py-2" key={pilot.id}>
-            <span className="text-black">{pilot.name}</span>
-            <button className="text-blue-500">Ver detalles</button>
+            <span className="text-red-500 font-semibold">{pilot.name}</span>
+            <button
+              className="text-blue-500"
+              onClick={() => setSelectedPilot(pilot)}
+            >
+              Ver detalles
+            </button>
           </li>
         ))}
       </ul>
+      {selectedPilot && <PilotDetails {...selectedPilot} />}
     </div>
   );
 };
