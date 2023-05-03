2a5c4cb714cd49b1618c7240773cf606e2293d9e
DALGO0927 <dieleon13@gmail.com>
2023-05-02 22:41:03 -0500

    cambios nuevos

--- pages/mainCompany/PilotList.tsx
+++ pages/mainCompany/PilotList.tsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+interface Pilot {
+  name: string;
+  id: string;
+}
 
 const PilotList: React.FC = () => {
+  const [pilots, setPilots] = useState<Pilot[]>([]);
+
+  useEffect(() => {
+    async function fetchPilots() {
+      const response = await fetch('http://localhost:3000/api/pilot');
+      const data = await response.json();
+      setPilots(data);
+    }
+    fetchPilots();
+  }, []);
+
   return (
     <div className="flex flex-col items-center space-y-4">
       <h2 className="text-lg font-semibold text-white">Lista de pilotos</h2>
       <ul className="divide-y divide-white">
-        <li className="flex justify-between py-2">
-          <span className="text-black">John Doe</span>
-          <button className="text-blue-500">Ver detalles</button>
-        </li>
-        <li className="flex justify-between py-2">
-          <span className="text-black">Jane Smith</span>
-          <button className="text-blue-500">Ver detalles</button>
-        </li>
-        <li className="flex justify-between py-2">
-          <span className="text-black">Bob Johnson</span>
-          <button className="text-blue-500">Ver detalles</button>
-        </li>
+        {pilots.map((pilot) => (
+          <li className="flex justify-between py-2" key={pilot.id}>
+            <span className="text-black">{pilot.name}</span>
+            <button className="text-blue-500">Ver detalles</button>
+          </li>
+        ))}
       </ul>
     </div>
   );
