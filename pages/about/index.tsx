fdb9d5e704700bf789fee256924b59ef0f7d0c1a
Gullit Moran <gullitmoran@gmail.com>
2023-05-08 11:11:28 -0500

    Agregando Iconos de Git y Link

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,23 +1,76 @@
 'use client';
 
-import { motion } from "framer-motion";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { motion } from "framer-motion";
 
 type Integrante = {
     nombre: string;
     rol: string;
     image: string;
+    github: string;
+    linkedin: string
 };
 
 const integrantes: Integrante[] = [
-    { nombre: "Luis Roca", rol: "Henry Mentor", image: "https://i.pravatar.cc/150?img=8" },
-    { nombre: "Hector Rios", rol: "Project Manager", image: "https://i.pravatar.cc/150?img=11" },
-    { nombre: "Esteban Ramirez", rol: "Desarrollador Frontend", image: "https://i.pravatar.cc/150?img=14" },
-    { nombre: "Martin Resnicoff", rol: "Desarrollador Frontend", image: "https://i.pravatar.cc/150?img=52" },
-    { nombre: "Diego Leon", rol: "Desarrollador Frontend", image: "https://i.pravatar.cc/150?img=3" },
-    { nombre: "Samir Diaz", rol: "Desarrollador Backend", image: "https://i.pravatar.cc/150?img=59" },
-    { nombre: "Leonardo Alabart", rol: "Desarrollador Backend", image: "https://i.pravatar.cc/150?img=18" },
-    { nombre: "Gullit Moran", rol: "Desarrollador Backend", image: "https://i.pravatar.cc/150?img=13" },
+    {
+        nombre: "Luis Roca",
+        rol: "Henry Mentor",
+        image: "https://i.pravatar.cc/150?img=8",
+        github: "https://github.com/luisroca88",
+        linkedin: "https://www.linkedin.com/in/luisroca88/"
+    },
+    {
+        nombre: "Hector Rios",
+        rol: "Project Manager",
+        image: "https://i.pravatar.cc/150?img=11",
+        github: "https://github.com/RiosHectorM",
+        linkedin: "https://www.linkedin.com/in/rioshectormartin/"
+    },
+    {
+        nombre: "Esteban Ramirez",
+        rol: "Desarrollador Frontend",
+        image: "https://i.pravatar.cc/150?img=14",
+        github: "https://github.com/luisroca88",
+        linkedin: "https://www.linkedin.com/in/luisroca88/"
+    },
+    {
+        nombre: "Martin Resnicoff",
+        rol: "Desarrollador Frontend",
+        image: "https://i.pravatar.cc/150?img=52",
+        github: "https://github.com/mresnicoff",
+        linkedin: "https://www.linkedin.com/in/martinresnicoff/"
+    },
+    {
+        nombre: "Diego Leon",
+        rol: "Desarrollador Frontend",
+        image: "https://i.pravatar.cc/150?img=3",
+        github: "https://github.com/DALGO0927",
+        linkedin: "https://www.linkedin.com/in/diegoleon09/"
+    },
+    {
+        nombre: "Samir Diaz",
+        rol: "Desarrollador Backend",
+        image: "https://i.pravatar.cc/150?img=59",
+        github: "https://github.com/SamirAlexander",
+        linkedin: "https://www.linkedin.com/in/luisroca88/"
+    },
+    {
+        nombre: "Leonardo Alabart",
+        rol: "Desarrollador Backend",
+        image: "https://i.pravatar.cc/150?img=18",
+        github: "https://github.com/Leonbart",
+        linkedin: "https://www.linkedin.com/in/leonardo-alabart-80067813/"
+    },
+    {
+        nombre: "Gullit Moran",
+        rol: "Desarrollador Backend",
+        image: "https://i.pravatar.cc/150?img=13",
+        github: "https://github.com/morangullit",
+        linkedin: "https://www.linkedin.com/in/gullit-enrique-moran-escobar-731272a5"
+    },
+
 
 ];
 
@@ -46,7 +99,7 @@ const About: React.FC = () => {
             </div>
 
             <div className="flex justify-center mt-24">
-                <div className="w-1/2 p-8 bg-gray-900 text-white transform scaleY(-1) shadow-xl text-center rounded-md" style={{opacity: 0.9}}>
+                <div className="w-1/2 p-8 bg-gray-900 text-white transform scaleY(-1) shadow-xl text-center rounded-md" style={{ opacity: 0.9 }}>
                     <p className="first-line:uppercase first-line:tracking-widest
                         first-letter:text-7xl first-letter:font-bold first-letter:text-white
                         first-letter:mr-3 first-letter:float-left text-justify">
@@ -74,10 +127,17 @@ const About: React.FC = () => {
                         <img src={integrante.image} alt={integrante.nombre} className="mx-auto rounded-full mb-4" />
                         <h2 className="text-2xl font-bold text-gray-800 mb-2">{integrante.nombre}</h2>
                         <p className="text-lg text-gray-600">{integrante.rol}</p>
+                        <div className="flex justify-center mt-4">
+                            <a href={integrante.github} target="_blank" rel="noopener noreferrer" className="mr-4">
+                                <FontAwesomeIcon icon={faGithub} size="2x" />
+                            </a>
+                            <a href={integrante.linkedin} target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                            </a>
+                        </div>
                     </div>
                 ))}
             </div>
-
 
         </div>
     )
