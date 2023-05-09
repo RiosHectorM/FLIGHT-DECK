0607f0979f290e80ff46c426e3ffcb99b39355f1
Gullit Moran <gullitmoran@gmail.com>
2023-05-09 16:07:38 -0500

    Agregando dash de Comp y Instr

--- pages/mainCompany/DashBoardCompany.tsx
+++ pages/mainCompany/DashBoardCompany.tsx
@@ -1,8 +1,10 @@
 import Head from 'next/head';
-import Link from 'next/link';
 import Main from './Main';
 import PilotList from './PilotList';
 import React, { useState } from 'react';
+import { FormPassword } from './form/formpassword';
+import { FormPhoto } from './form/formphoto';
+import { FormCompany } from './form/formcompany';
 
 const DashboardCompany = () => {
   const [showPilots, setShowPilots] = useState(false);
@@ -13,14 +15,13 @@ const DashboardCompany = () => {
 
   return (
     <div
-    className="min-h-screen items-center justify-center p-8"
-    style={{
-      backgroundImage: "url('/images/DASHCOMPANY.jpg')",
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-    }}
-  >
-  
+      className="min-h-screen items-center justify-center p-8"
+      style={{
+        backgroundImage: "url('/images/DASHCOMPANY.jpg')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="My Company Dashboard" />
@@ -28,6 +29,21 @@ const DashboardCompany = () => {
         <meta name="author" content="My Company" />
         <title>Dashboard - My Company</title>
       </Head>
+      <div className="flex justify-center">
+        <div className="bg-white bg-opacity-70 rounded-lg shadow-lg p-6 flex flex-wrap flex-row">
+          <h2 className="text-xl font-bold mb-10 w-full">Actualizar perfil de la compañía</h2>
+          <div className="mx-auto max-w-md">
+            <FormPhoto />
+          </div>
+          <div className="mx-auto max-w-md">
+            <FormCompany />
+          </div>
+          <div className="mx-auto max-w-md">
+            <FormPassword />
+          </div>
+        </div>
+      </div>
+
       <h1 className="text-3xl font-bold leading-tight text-white mt-5">
         Dashboard
       </h1>
@@ -38,12 +54,9 @@ const DashboardCompany = () => {
         {showPilots ? 'Ocultar pilotos' : 'Ver pilotos registrados'}
       </button>
       {showPilots && <PilotList />}
-      <Main/>
+      <Main />
     </div>
   );
 };
 
 export default DashboardCompany;
-
-
-
