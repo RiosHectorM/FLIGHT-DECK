eb9e0db5ca5a8d61bf583ce31caeb230ad8dc2d9
Esteban7712 <bleed.7712@gmail.com>
2023-05-08 21:47:07 -0500

    about styles

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,23 +1,67 @@
 'use client';
 
-import { motion } from "framer-motion";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { motion } from "framer-motion";
 
 type Integrante = {
     nombre: string;
     rol: string;
     image: string;
+    github: string;
+    linkedin: string
 };
 
 const integrantes: Integrante[] = [
-    { nombre: "Luis Roca", rol: "Henry Mentor", image: "https://i.pravatar.cc/150?img=8" },
-    { nombre: "Hector Rios", rol: "Project Manager", image: "https://i.pravatar.cc/150?img=11" },
-    { nombre: "Esteban Ramirez", rol: "Desarrollador Frontend", image: "https://i.pravatar.cc/150?img=14" },
-    { nombre: "Martin Resnicoff", rol: "Desarrollador Frontend", image: "https://i.pravatar.cc/150?img=52" },
-    { nombre: "Diego Leon", rol: "Desarrollador Frontend", image: "https://i.pravatar.cc/150?img=3" },
-    { nombre: "Samir Diaz", rol: "Desarrollador Backend", image: "https://i.pravatar.cc/150?img=59" },
-    { nombre: "Leonardo Alabart", rol: "Desarrollador Backend", image: "https://i.pravatar.cc/150?img=18" },
-    { nombre: "Gullit Moran", rol: "Desarrollador Backend", image: "https://i.pravatar.cc/150?img=13" },
+    { nombre: "Luis Roca",
+      rol: "Henry Mentor",
+      image: "https://media.licdn.com/dms/image/D4E35AQFAcfkPHfpvHA/profile-framedphoto-shrink_200_200/0/1678131873780?e=1684206000&v=beta&t=zwiP3P51ot4JgLACK4WvEwAvdfLcq2EBIrj1cMnY1fc",
+      github: "https://github.com/LuisRocca",
+      linkedin: "https://www.linkedin.com/in/luis-miguel-alfonzo-roca-software-enginer/" 
+    },
+    { nombre: "Hector Rios",
+      rol: "Project Manager", 
+      image: "https://media.licdn.com/dms/image/D4D35AQGLmLJrw_gGzQ/profile-framedphoto-shrink_200_200/0/1662646665729?e=1684108800&v=beta&t=bht4F3iB9hV--tohRHaaz7NcfIsQhiwonrC2ZBY4_lA",
+      github: "https://github.com/RiosHectorM",
+      linkedin: "https://www.linkedin.com/in/rioshectormartin/"
+    },
+    { nombre: "Esteban Ramirez", 
+      rol: "Frontend Developer", 
+      image: "https://media.licdn.com/dms/image/D4E03AQEYlKm3F7RbuQ/profile-displayphoto-shrink_200_200/0/1683505683094?e=1689206400&v=beta&t=oc375UOXMQdArWrryuOkcEPHr--Kda-bXTTSL-vfNUs", 
+      github: "https://github.com/esteban7712",
+      linkedin: "https://www.linkedin.com/in/esteban7712/"
+    },
+    { nombre: "Martin Resnicoff", 
+      rol: "Frontend Developer", 
+      image: "https://media.licdn.com/dms/image/D4D35AQE432SnKtmJ8w/profile-framedphoto-shrink_200_200/0/1677156557692?e=1684108800&v=beta&t=T6tT9oftdCCZhKCkTaEiItknOmxzmDcjn_ccMkT-QNs", 
+      github: "https://github.com/mresnicoff",
+      linkedin: "https://www.linkedin.com/in/martinresnicoff/"
+    },
+    { nombre: "Diego Leon", 
+      rol: "Frontend Developer", 
+      image: "https://media.licdn.com/dms/image/D4D35AQHe-6-KOnIqJA/profile-framedphoto-shrink_200_200/0/1678240089759?e=1684198800&v=beta&t=bzxEZits89nD2NWOaN2_1y8jFUiimX0XDzTHy02cC2g",
+      github: "https://github.com/DALGO0927",
+      linkedin: "https://www.linkedin.com/in/diegoleon09/"
+    },
+    { nombre: "Samir Diaz", 
+      rol: "Backend Developer", 
+      image: "https://media.licdn.com/dms/image/D4E03AQHiiNsEj8-90Q/profile-displayphoto-shrink_200_200/0/1675523925928?e=1689206400&v=beta&t=2_ceOQTMWGKP8gsAlRDngmzMHo2Ti7ZdrXyAVt2BzrA",
+      github: "https://github.com/SamirAlexander",
+      linkedin: "https://www.linkedin.com/in/samiralexanderdiaz/"
+    },
+    { nombre: "Leonardo Alabart", 
+      rol: "Backend Developer", 
+      image: "https://media.licdn.com/dms/image/C4E03AQGkCvWol3snfw/profile-displayphoto-shrink_200_200/0/1647807298164?e=1689206400&v=beta&t=D_lhKfg7sMgxcJ3t0iCSs3QTi6m5Q0fJzstXkKfpSJE",
+      github: "https://github.com/Leonbart",
+      linkedin: "https://www.linkedin.com/in/leonardo-alabart-80067813/"
+    },
+    { nombre: "Gullit Moran", 
+      rol: "Backend Developer", 
+      image: "https://media.licdn.com/dms/image/D4E03AQHHEsf7qnoV3g/profile-displayphoto-shrink_200_200/0/1677531330933?e=1689206400&v=beta&t=yhdsycFXs69Ksk4rUwq5FJSG6zGfu6PrbV59uKM2PKA",
+      github: "https://github.com/morangullit",
+      linkedin: "https://www.linkedin.com/in/gullit-enrique-moran-escobar-731272a5"
+    },
 
 ];
 
@@ -46,18 +90,18 @@ const About: React.FC = () => {
             </div>
 
             <div className="flex justify-center mt-24">
-                <div className="w-1/2 p-8 bg-gray-900 text-white transform scaleY(-1) shadow-xl text-center rounded-md" style={{opacity: 0.9}}>
-                    <p className="first-line:uppercase first-line:tracking-widest
-                        first-letter:text-7xl first-letter:font-bold first-letter:text-white
+                <div className="w-1/2 p-8 text-lg bg-white text-black transform scaleY(-1) shadow-xl text-center rounded-md bg-opacity-60" >
+                    <p className="
+                        first-letter:text-7xl first-letter:font-bold first-letter:text-black
                         first-letter:mr-3 first-letter:float-left text-justify">
-                        Nuestro equipo ha desarrollado una Bitacora de Vuelo (LogBook)
-                        donde los pilotos pueden tener un soporte digital y online
-                        de sus horas de vuelo ya sean certificadas o no por un Instructor.
+                        Our team has developed a Flight LogBook where pilots can have digital
+                        and online support for their flight hours, whether certified
+                        or not by an Instructor.
                         <br />
                         <br />
-                        Los Pilotos podran ser visibles para las Empresas registradas e incluso
-                        podran formar parte del staff de dichas Empresas y de esa forma poder
-                        obtener la certificacion de sus horas sin necesidad de un Instructor.
+                        Pilots can be visible to registered companies and even become part of 
+                        the staff of such companies, thus being able to obtain certification
+                        of their hours without the need for an Instructor.
                     </p>
                 </div>
             </div>
@@ -70,10 +114,18 @@ const About: React.FC = () => {
 
             <div className="grid grid-cols-3 gap-8">
                 {integrantes.map((integrante) => (
-                    <div key={integrante.nombre} className="bg-white shadow-md rounded-lg p-6 text-center">
-                        <img src={integrante.image} alt={integrante.nombre} className="mx-auto rounded-full mb-4" />
-                        <h2 className="text-2xl font-bold text-gray-800 mb-2">{integrante.nombre}</h2>
-                        <p className="text-lg text-gray-600">{integrante.rol}</p>
+                    <div key={integrante.nombre} className="bg-white shadow-xl rounded-lg p-6 text-center bg-opacity-60">
+                        <img src={integrante.image} alt={integrante.nombre} className="mx-auto shadow-xl rounded-full mb-4" />
+                        <h2 className="text-2xl font-bold text-black-800 mb-2">{integrante.nombre}</h2>
+                        <p className="text-lg text-gray-800">{integrante.rol}</p>
+                        <div className="flex justify-center mt-4">
+                            <a href={integrante.github} target="_blank" rel="noopener noreferrer" className="mr-4">
+                                <FontAwesomeIcon icon={faGithub} size="2x" />
+                            </a>
+                            <a href={integrante.linkedin} target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                            </a>
+                        </div>
                     </div>
                 ))}
             </div>
