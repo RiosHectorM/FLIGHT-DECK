5476a8d6f3d7865cdcb9e96778dc23ae4f1e06bf
RiosHectorM <rios.hector.martin@gmail.com>
2023-05-11 05:56:17 -0300

    'deploy3'

--- pages/api/flight/setCertified/[id].ts
+++ pages/api/flight/setCertified/[id].ts
@@ -7,37 +7,30 @@ export default async function handler(
 ) {
   // - PUT --- Set certified field (to true or false) ---------------
   if (req.method === 'PUT') {
-    const { id } = req.query;
-    const { certified } = req.body;
-    
-    // Verify existence of required fields
-    if (
-      !id ||
-      !certified
-    ) {
+    const { id } = req.query as { id: string };
+    const { certified } = req.body as { certified: boolean };
 
+    // Verify existence of required fields
+    if (!id || !certified) {
       return res.status(400).json({ message: 'Required field missing' });
     }
 
     try {
       const flight = await prisma.flight.update({
-        where: {id},
+        where: { id },
         data: {
-          certified
-      }});
+          certified,
+        },
+      });
       return res.status(201).json(flight);
-    }
-    catch (error) {
+    } catch (error) {
       // Handle flight creation error
       console.error(error);
       return res.status(500).json({ message: `Error updating flight` });
-    }
-    finally {
+    } finally {
       await prisma.$disconnect();
     }
-  }
-
-  else {
+  } else {
     return res.status(405).json({ message: 'Method not allowed' });
   }
 }
