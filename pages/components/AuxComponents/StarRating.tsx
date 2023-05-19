7dd5104f4a6d27bcf264dfc55ef96a828c891ea6
Leonardo Alabart <leoanrdo.alabart@gmail.com>
2023-05-19 14:38:11 -0300

    Cambios Martín

---
+++ pages/components/AuxComponents/StarRating.tsx
@@ -0,0 +1,25 @@
+import {useState} from 'react'
+const StarRating = ({rating,rankear}:{rating:number; rankear:(index:number)=>void } ) => {
+
+    const [hover, setHover] = useState(0)
+    return (
+      <div className="star-rating">
+        {[...Array(5)].map((star, index) => {
+          index += 1;
+          return (
+            <button
+              type="button"
+              key={index}
+              className={index <= (hover || rating) ? "bg-transparent border-0 outline-0 cursor-pointer text-yellow-500 font-black" : "bg-transparent border-0 outline-0 cursor-pointer font-black"}
+              onClick={() => rankear(index)}
+              onMouseEnter={() => setHover(index)}
+              onMouseLeave={() => setHover(rating)}
+            >
+              <span className="star">&#9733;</span>
+            </button>
+          );
+        })}
+      </div>
+    );
+  };
+  export default StarRating;
