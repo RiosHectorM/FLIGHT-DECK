8ba5c411b99d4bc70dac0fe0e2866f18a42469b5
Leo <leonardo.alabart@gmail.com>
2023-05-20 01:58:21 -0300

    feat(charts): Work in progress

--- pages/dashboardPilot/FlightStats.tsx
+++ pages/dashboardPilot/FlightStats.tsx
@@ -1,22 +1,75 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import * as echarts from "echarts/core";
+import { BarChart, LineChart } from "echarts/charts";
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  // Dataset
+  DatasetComponent,
+  // Built-in transform (filter, sort)
+  TransformComponent,
+} from "echarts/components";
+import { LabelLayout, UniversalTransition } from "echarts/features";
+import { CanvasRenderer } from "echarts/renderers";
+import type {
+  // The series option types are defined with the SeriesOption suffix
+  BarSeriesOption,
+  LineSeriesOption,
+} from "echarts/charts";
+import type {
+  // The component option types are defined with the ComponentOption suffix
+  TitleComponentOption,
+  TooltipComponentOption,
+  GridComponentOption,
+  DatasetComponentOption,
+} from "echarts/components";
+import type { ComposeOption } from "echarts/core";
 
 interface Props {
   userId?: string;
 }
+// Create an Option type with only the required components and charts via ComposeOption
+type ECOption = ComposeOption<
+  | BarSeriesOption
+  | LineSeriesOption
+  | TitleComponentOption
+  | TooltipComponentOption
+  | GridComponentOption
+  | DatasetComponentOption
+>;
+
+// Register the required components
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  BarChart,
+  LineChart,
+  LabelLayout,
+  UniversalTransition,
+  CanvasRenderer,
+]);
+
+const option: ECOption = {
+  // ...
+};
 
 const FlightStats = ({ userId }: Props) => {
-  const [certifiedHours, setCertifiedHours] = useState<number>(0);
+  // const [certifiedHours, setCertifiedHours] = useState<number>(0);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        if (userId !== undefined){
+        if (userId !== undefined) {
           const response = await axios.get(
             `/api/pilot/getHoursByUserId/${userId}`
           );
           const data = response.data;
-          setCertifiedHours(data.CertifiedHours || 0);
+          // setCertifiedHours(data.CertifiedHours || 0);
         }
       } catch (error) {
         console.error(error);
@@ -28,7 +81,7 @@ const FlightStats = ({ userId }: Props) => {
 
   return (
     <div>
-      <p>{certifiedHours}</p>
+      <p>{userId}</p>
     </div>
   );
 };
--- pages/dashboardPilot/MainPiloto.tsx
+++ pages/dashboardPilot/MainPiloto.tsx
@@ -1,24 +1,23 @@
-import { useEffect, useState } from 'react';
-import FormPilot from './formpilot';
-import FormPassword from './formpassword';
-import Chat from '../mainInstructor/Chat';
-import ProfileSection from '../mainInstructor/ProfileSection';
+import { useEffect, useState } from "react";
+import FormPilot from "./formpilot";
+import FormPassword from "./formpassword";
+import Chat from "../mainInstructor/Chat";
+import ProfileSection from "../mainInstructor/ProfileSection";
 
-import Notification from '../mainInstructor/Notification';
-import FormPhoto from './formphoto';
-import ChatComponent from '../Chat';
-import { useSession } from 'next-auth/react';
-import { useUserStore } from '@/store/userStore';
-import ModalComponent from './certificaMedicPilot';
-import LicensePilot from './licensePilot';
-
+import Notification from "../mainInstructor/Notification";
+import FormPhoto from "./formphoto";
+import ChatComponent from "../Chat";
+import { useSession } from "next-auth/react";
+import { useUserStore } from "@/store/userStore";
+import ModalComponent from "./certificaMedicPilot";
+import LicensePilot from "./licensePilot";
+import FlightStats from "./FlightStats";
 
 const MainPiloto = () => {
-  
-/*   const [showProfile, setShowProfile] = useState(false);
+  /*   const [showProfile, setShowProfile] = useState(false);
   const [showChat, setShowChat] = useState(false); */
 
-/*   const handleShowProfile = () => {
+  /*   const handleShowProfile = () => {
     setShowProfile(!showProfile);
   }; */
 
@@ -30,8 +29,6 @@ const MainPiloto = () => {
 
   const { user, fetchUserByEmail } = useUserStore();
 
- 
-
   useEffect(() => {
     if (data?.user?.email) {
       console.log(data.user.email);
@@ -42,46 +39,46 @@ const MainPiloto = () => {
 
   return (
     <div
-      className='min-h-screen bg-gray-100'
+      className="min-h-screen bg-gray-100"
       style={{
         backgroundImage: "url('/images/pilotomain.jpg')",
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
       }}
     >
       {user?.id && (
-        <main className='py-10'>
+        <main className="py-10">
           <div className="mx-auto max-w-full px-4 sm:px-6 lg:px-8">
             <div className="px-4 py-6 sm:px-0">
               <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-              <div className='bg-white rounded-xl shadow-md'>
-              <ModalComponent/>
-              </div>
-              <div className='bg-white rounded-xl shadow-md'>
-              <LicensePilot/>
-              </div>
-              <div className='bg-white rounded-xl shadow-md'>
-              <ModalComponent/>
-              </div>
+                <div className="bg-white rounded-xl shadow-md">
+                  <ModalComponent />
+                </div>
+                <div className="bg-white rounded-xl shadow-md">
+                  <LicensePilot />
+                </div>
+                <div className="bg-white rounded-xl shadow-md">
+                  <ModalComponent />
+                </div>
               </div>
             </div>
           </div>
-          <div className='mx-auto max-w-full px-4 sm:px-6 lg:px-8'>
-            <div className='flex flex-col lg:flex-row'>
-              <div className='lg:w-full mr-10'>
-                <div className='bg-white bg-opacity-70 rounded-lg shadow-lg p-6 mb-6'>
-                  <h2 className='text-xl font-bold mb-10 w-full'>
+          <div className="mx-auto max-w-full px-4 sm:px-6 lg:px-8">
+            <div className="flex flex-col lg:flex-row">
+              <div className="lg:w-full mr-10">
+                <div className="bg-white bg-opacity-70 rounded-lg shadow-lg p-6 mb-6">
+                  <h2 className="text-xl font-bold mb-10 w-full">
                     Pilot Information
                   </h2>
-                  <div className='grid grid-cols-1 md:grid-cols-3 gap-4 max-w-full mx-auto'>
-                    <div className='mx-auto max-w-80 h-80'>
+                  <div className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-full mx-auto">
+                    <div className="mx-auto max-w-80 h-80">
                       <FormPhoto />
                     </div>
-                    <div className='mx-auto max-w-md rounded-xl'>
+                    <div className="mx-auto max-w-md rounded-xl">
                       <FormPilot />
                     </div>
-                    <div className='mx-auto max-w-md'>
+                    <div className="mx-auto max-w-md">
                       <FormPassword />
                     </div>
                   </div>
@@ -122,6 +119,15 @@ const MainPiloto = () => {
                 )}
             )} 
               </div> */}
+          </div>
+          <div className="mx-auto max-w-full px-4 sm:px-6 lg:px-8">
+            <div className="px-4 py-6 sm:px-0">
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                <div className="bg-white rounded-xl shadow-md">
+                  <FlightStats userId={user.id} />
+                </div>
+              </div>
+            </div>
           </div>
         </main>
       )}
