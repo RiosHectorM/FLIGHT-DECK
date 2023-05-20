4836561f44b43d51f7fee997ee7f8977a441e4c7
RiosHectorM <rios.hector.martin@gmail.com>
2023-05-20 19:57:29 -0300

    'router next/navigation'

--- pages/components/AuxComponents/ProtectedRoute.tsx
+++ pages/components/AuxComponents/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useEffect, ReactNode } from "react";
 import { useUserStore, UserRole } from '../../../store/userStore';
 
