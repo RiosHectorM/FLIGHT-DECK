ef43abf22be6d81cfc3906b11f1d7ca8e253d2e2
DALGO0927 <dieleon13@gmail.com>
2023-05-23 23:18:06 -0500

    estilos menu item main piloto dasboard company y el loader

--- pages/components/Loader/index.tsx
+++ pages/components/Loader/index.tsx
@@ -3,9 +3,9 @@ import { BeatLoader } from "react-spinners";
 
 const Loader = () => {
   return (
-    <div className="fixed inset-0 flex items-center z-50 focus:outline-none bg-violet-200/70 animated-gradient">
-      <div className="mx-auto p-8 rounded-3xl shadow-xl bg-white">
-        <BeatLoader color={"#000000"} loading={true} />
+    <div className="fixed inset-0 flex items-center z-50 focus:outline-none bg-yellow-100/70 animated-gradient">
+      <div className="mx-auto p-8 rounded-3xl shadow-xl bg-flightdeck-cream">
+        <BeatLoader color={"black"} loading={true} />
       </div>
     </div>
   );
--- pages/mainCompany/DashBoardCompany.tsx
+++ pages/mainCompany/DashBoardCompany.tsx
@@ -33,13 +33,13 @@ const DashboardCompany: FC = () => {
     setShowFormPassword(true);
   };
 
-    const handleCertifications = (
-      event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-    ) => {
-      event.preventDefault();
-      setShowInfo(false);
-      setShowCertificates(true);
-    };
+  const handleCertifications = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    setShowInfo(false);
+    setShowCertificates(true);
+  };
 
   useEffect(() => {
     if (data?.user?.email) {
@@ -51,99 +51,111 @@ const DashboardCompany: FC = () => {
 
   return (
     //<ProtectedRoute allowedRoles={['INSTRUCTOR']}>
-      <div
-        className='min-h-screen bg-gray-100 w-full'
-        style={{
-          backgroundImage: "url('/images/DASHCOMPANY.jpg')",
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      >
-        {user?.id && (
-          <main>
-            <ToasterProvider />
-            <div className='max-w-7xl mx-auto py-6 sm:px-6 lg:px-8'>
-              <div className='flex justify-center'>
-                <div className='bg-white bg-opacity-70 rounded-lg shadow-lg p-6 w-11/12  md:w-4/5 justify-center'>
-                  <h2 className='text-xl font-bold mb-10 w-full text-center'>
-                    INSTRUCTOR INFORMATION
-                  </h2>
-                  <div className='flex flex-col lg:flex-row gap-4 max-w-full items-center '>
-                    <div className='flex flex-col lg:flex-row w-full'>
-                      <div className='mx-auto h-auto mb-10 max-w-4/5 lg:max-w-4/5'>
-                        <FormPhoto />
-                      </div>
-                      {showInfo && (
-                        <div className='flex flex-col text-xl text-center lg:text-left'>
-                          <p className='font-bold text-2xl'>{user.name}</p>
-                          <p className='font-bold text-2xl'>{user.lastName}</p>
-                          <p className='font-semibold italic mb-2 mt-6'>
-                            {user.email}
-                          </p>
-                          <p className='font-semibold italic mb-2 mt-6 flex'>
-                            <p className='font-normal mr-2 not-italic'>
-                              Nacionality:{' '}
-                            </p>
-                            {user.nationality}
-                          </p>
-                          <p className='font-semibold italic mb-2 mt-2 flex'>
-                            <p className='font-normal mr-2 not-italic'>
-                              City:{' '}
-                            </p>{' '}
-                            {user.city}
-                          </p>
-                          <p className='font-semibold italic mb-2 mt-2 flex'>
-                            <p className='font-normal mr-2 not-italic'>
-                              Address:{' '}
-                            </p>
-                            {user.address}
-                          </p>
-                          <p className='font-semibold italic mb-2 mt-2 flex'>
-                            <p className='font-normal mr-2 not-italic'>
-                              Phone:{' '}
-                            </p>
-                            {user.phoneNumber}
-                          </p>
-                          <button
-                            className='font-sans bg-gray-800 text-white my-4 rounded-md py-2 hover:bg-gray-400 hover:text-black hover:font-bold hover:border hover:border-black'
-                            onClick={handleFormInstructor}
-                          >
-                            Edit Information
-                          </button>
-                          <button
-                            className='font-sans bg-gray-800 text-white mb-4 rounded-md py-2 hover:bg-gray-400 hover:text-black hover:font-bold hover:border hover:border-black'
-                            onClick={handleFormPassword}
-                          >
-                            Change Password
-                          </button>
-                        </div>
-                      )}
+    <div
+      className='min-h-screen bg-gray-100 w-full'
+      style={{
+        backgroundImage: "url('/images/DASHCOMPANY.jpg')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      {user?.id && (
+        <main>
+          <ToasterProvider />
+          <div className='max-w-7xl mx-auto py-6 sm:px-6 lg:px-8'>
+            <div className='flex justify-center'>
+              <div className='bg-white bg-opacity-70 rounded-lg shadow-lg p-6 w-11/12  md:w-4/5 justify-center'>
+                <h2 className='text-xl font-bold mb-10 w-full text-center'>
+                  INSTRUCTOR INFORMATION
+                </h2>
+                <div className='flex flex-col lg:flex-row gap-4 max-w-full items-center '>
+                  <div className='flex flex-col lg:flex-row w-full'>
+                    <div className='mx-auto h-auto mb-10 max-w-4/5 lg:max-w-4/5'>
+                      <FormPhoto />
                     </div>
-                    {showFormInstructor && (
-                      <div className='mx-auto h-auto mb-10 w-full lg:w-4/5'>
-                        <FormCompany
-                          setShowInfo={setShowInfo}
-                          setShowFormInstructor={setShowFormInstructor}
-                        />
-                      </div>
-                    )}
-                    {showFormPassword && (
-                      <div className='mx-auto h-auto mb-10 w-full lg:w-4/5'>
-                        <FormPassword
-                          setShowInfo={setShowInfo}
-                          setShowFormPassword={setShowFormPassword}
-                        />
+                    {showInfo && (
+                      <div
+                        className='flex flex-col text-xl text-center lg:text-left'
+                        style={{
+                          color: '#1a1a1a', // flightdeck-dark
+                        }}
+                      >
+                        <p className='font-bold text-2xl'>{user.name}</p>
+                        <p className='font-bold text-2xl'>{user.lastName}</p>
+                        <p className='font-semibold italic mb-2 mt-6'>
+                          {user.email}
+                        </p>
+                        <p className='font-semibold italic mb-2 mt-6 flex'>
+                          <p className='font-normal mr-2 not-italic'>
+                            Nacionality:{' '}
+                          </p>
+                          {user.nationality}
+                        </p>
+                        <p className='font-semibold italic mb-2 mt-2 flex'>
+                          <p className='font-normal mr-2 not-italic'>
+                            City:{' '}
+                          </p>{' '}
+                          {user.city}
+                        </p>
+                        <p className='font-semibold italic mb-2 mt-2 flex'>
+                          <p className='font-normal mr-2 not-italic'>
+                            Address:{' '}
+                          </p>
+                          {user.address}
+                        </p>
+                        <p className='font-semibold italic mb-2 mt-2 flex'>
+                          <p className='font-normal mr-2 not-italic'>
+                            Phone:{' '}
+                          </p>
+                          {user.phoneNumber}
+                        </p>
+                        <button
+                          className='font-sans bg-gray-800 text-white my-4 rounded-md py-2 hover:bg-gray-400 hover:text-black hover:font-bold hover:border hover:border-black'
+                          onClick={handleFormInstructor}
+                          style={{
+                            backgroundColor: '#1a1a1a', // flightdeck-dark
+                            color: '#e5d9b6', // flightdeck-cream
+                          }}
+                        >
+                          Edit Information
+                        </button>
+                        <button
+                          className='font-sans bg-gray-800 text-white mb-4 rounded-md py-2 hover:bg-gray-400 hover:text-black hover:font-bold hover:border hover:border-black'
+                          onClick={handleFormPassword}
+                          style={{
+                            backgroundColor: '#1a1a1a', // flightdeck-dark
+                            color: '#e5d9b6', // flightdeck-cream
+                          }}
+                        >
+                          Change Password
+                        </button>
                       </div>
                     )}
                   </div>
+                  {showFormInstructor && (
+                    <div className='mx-auto h-auto mb-10 w-full lg:w-4/5'>
+                      <FormCompany
+                        setShowInfo={setShowInfo}
+                        setShowFormInstructor={setShowFormInstructor}
+                      />
+                    </div>
+                  )}
+                  {showFormPassword && (
+                    <div className='mx-auto h-auto mb-10 w-full lg:w-4/5'>
+                      <FormPassword
+                        setShowInfo={setShowInfo}
+                        setShowFormPassword={setShowFormPassword}
+                      />
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
-          </main>
-        )}
-      </div>
+          </div>
+        </main>
+      )}
+    </div>
 
-    
     //</ProtectedRoute>
   );
 };
