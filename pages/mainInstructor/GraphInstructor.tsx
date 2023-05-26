18f03741bc5ad077903c6dd4c4262e2550aef952
Leo <leonardo.alabart@gmail.com>
2023-05-26 01:48:16 -0300

    fix(charts): Created new page for instructor's stats called GraphInstructor

--- pages/mainInstructor/GraphInstructor.tsx
+++ pages/mainInstructor/GraphInstructor.tsx
@@ -1,11 +1,6 @@
 import { FC, useEffect, useState } from "react";
-import FormPhoto from "./form/formphoto";
-import FromInstructor from "./form/forminstructor";
-import FormPassword from "./form/formpassword";
 import { useSession } from "next-auth/react";
 import { useUserStore } from "@/store/userStore";
-import ToasterProvider from "../providers/ToasterProvider";
-import FormCertificates from "./form/formCertificates";
 import Loader from "../components/Loader";
 import StatsInstructor from "../components/AuxComponents/Stats/StatsInstructor";
 
@@ -15,40 +10,10 @@ const GraphInstructor: FC = () => {
   const { user, fetchUserByEmail } = useUserStore();
 
   const [isLoading, setIsLoading] = useState(false);
-  // const [showFormInstructor, setShowFormInstructor] = useState(false);
-  // const [showFormPassword, setShowFormPassword] = useState(false);
-  // const [showCertificates, setShowCertificates] = useState(false);
-  // const [showInfo, setShowInfo] = useState(true);
-
-
-  // const handleFormInstructor = (
-  //   event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  // ) => {
-  //   event.preventDefault();
-  //   setShowInfo(false);
-  //   setShowFormInstructor(true);
-  // };
-
-  // const handleFormPassword = (
-  //   event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  // ) => {
-  //   event.preventDefault();
-  //   setShowInfo(false);
-  //   setShowFormPassword(true);
-  // };
-
-  // const handleCertifications = (
-  //   event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  // ) => {
-  //   event.preventDefault();
-  //   setShowInfo(false);
-  //   setShowCertificates(true);
-  // };
 
   useEffect(() => {
     setIsLoading(true);
     if (data?.user?.email) {
-      console.log(data.user.email);
       const email = data.user.email;
       fetchUserByEmail(email);
     }
@@ -67,7 +32,6 @@ const GraphInstructor: FC = () => {
       {isLoading && <Loader />}
       {user?.id && (
         <main>
-          <ToasterProvider />
           <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
             <div className="flex justify-center">
               <div className="bg-white bg-opacity-70 rounded-lg shadow-lg px-6 pt-6 pb-14 w-11/12  md:w-4/5 justify-center">
